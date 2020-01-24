f65eef45e8216cad145e5ee45eacc508084ced2c
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-01-24 18:38:23 -0500

    css additions for mobile adaptation

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,18 +7,12 @@ function Intro() {
         <div className="intro">
             <img className="photo" src={Photo}></img>
             <div className="info">
-                <h1 style={namefont}>Dennis Krupitsky</h1>
-                <h2 style={position}>Software Engineer</h2>
+                <h1 className="namefont">Dennis Krupitsky</h1>
+                <h2 className="position">Software Engineer</h2>
             </div>
         </div>
     )
 }
 
-const namefont = {
-    fontSize: '65px'
-}
 
-const position = {
-    fontSize: '45px'
-}
 export default Intro;
