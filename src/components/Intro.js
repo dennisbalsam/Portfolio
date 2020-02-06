39df209e2310f57af28a328a36d7e50cbaafbb88
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-06 16:57:23 -0500

    finsihed bio

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,24 +4,30 @@ import Photo from '../images/personal-image4.jpg'
 
 function Intro() {
     return (
-        <React.Fragment>        
-        <div className="intro col-sm-4 col-md-10 col-lg-12" id="intro">
-            <img className="photo" src={Photo}></img>
-            <div className="info">
-                <h1 className="namefont">Dennis Krupitsky</h1>
-                <h2 className="position">Software Engineer</h2>
+        <React.Fragment>   
+        <div className="intro" id="intro">     
+            <div className="col-sm-4 col-md-10 col-lg-12">
+                <img className="photo" src={Photo}></img>
+                <div className="info">
+                    <h1 className="namefont">Dennis Krupitsky</h1>
+                    <h2 className="position">Software Engineer</h2>
+                </div>
             </div>
-        </div>
-        <div>
-            <div className="blackbar float-right"></div>
-            <h2 className="aboutme"><span style={{color: 'rgb(4, 124, 34)'}}>ABOUT </span> ME</h2>
-            <div className="blackbar float-left"></div>
-            <h5 className="mx-auto" >Allow me to (re)introduce myself</h5>
-        </div>
-        <div className="description">
-        <p> 
-        Hi, nice to meet you, my name is Dennis.
-        </p>
+            <div>
+                <div className="blackbar float-right"></div>
+                <h2 className="headers"><span style={{color: 'rgb(4, 124, 34)'}}>ABOUT </span> ME</h2>
+                <div className="blackbar float-left"></div>
+                <h5 className="mx-auto message" >Allow me to (re)introduce myself</h5>
+            </div>
+            <div className="description">
+                <p className="bio"> 
+                    My name is Dennis, I am an aspiring software engineer in New York City. I am currently working on acquiring my Bachelor of Science degree in Computer Science at the College of Staten Island. 
+                    I have a passion for creating modern applications, in an attempt to become a full-stack developer. I am always looking to meet new people, learn new things, and
+                    make the most out of life. On a more personal note, I am a big fanatic of the NFL, MLB and NBA (my goal is to start sports podcast by the end of the year), and I also 
+                    really really love snowboarding. My time in college is coming to an end, and I am very excited to 
+                    see what is in store for me in the real world.
+                </p>
+            </div>
         </div>
         </React.Fragment>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-            <Nav.Link className="mynavlinks" href="#intro">Intro</Nav.Link>
-            <Nav.Link className="mynavlinks" href="#intro">Intro</Nav.Link>
+            <Nav.Link className="mynavlinks mr-4 ml-4" href="#intro">Intro</Nav.Link>
+            <Nav.Link className="mynavlinks mr-5 ml-5" href="#intro">Skills</Nav.Link>
             <div className="initials mx-auto">DK</div> 
-            <Nav.Link className="mynavlinks" href="#intro">Hello</Nav.Link>
-            <Nav.Link className="mynavlinks" href="#intro">Hello</Nav.Link>
+            <Nav.Link className="mynavlinks ml-5 mr-5" href="#intro">Projects</Nav.Link>
+            <Nav.Link className="mynavlinks ml-5" href="#intro">Contacts</Nav.Link>
         </Nav>
         </Navbar.Collapse>
 
