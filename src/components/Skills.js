8e874967308db092332c9c021254f97f41a3ab1d
Dennis Krupitsky <dennis.krupitsky@gmail.com>
2020-02-11 15:44:59 -0500

    starting skills

---
+++ src/components/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Skills() {
+    return (
+        <React.Fragment>
+            <div>
+            <div className="blackbar float-right"></div>
+                <h2 className="headers"><span style={{color: 'rgb(4, 124, 34)'}}>SKI</span>LLS</h2>
+                <div className="blackbar float-left"></div>
+            </div>
+        </React.Fragment> 
+    )
+}
