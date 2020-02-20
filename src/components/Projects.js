afee584927ea05d7f83fdaaf1b03eabd2bbc81cc
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-19 20:01:09 -0500

    almost done with project data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,8 +8,9 @@ class Projects extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        description: 'SPORTSHUB APP',
-        gif: SPORTSHUB
+        description: this.projects[0].description,
+        gif: this.projects[0].gif,
+        title: this.projects[0].title,
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -18,32 +19,37 @@ class Projects extends Component {
   projects = [
     {
         id: 1,
-        description: 'SPORTSHUB APP',
+        title:'SPORTSHUB',
+        description: 'Project creating a sports application utilizing the sportsdataio, firebase, and microsoft azure apis to create a sports betting application. The user is able to sign in to their google account, and from there they are able to view stats, highlights, standings, etc about multiple sports. They are able to see what upcoming games are scheduled by sport and allowed the oppurtunity to place a wager on that team. The user is able to deposit funds into their account, and also view bets they have placed.',
         gif: SPORTSHUB,
         gitlink:'link'
     },
     {
         id:2,
-        description: 'YELP APP',
+        description: 'Project utilizing the yelp, firebase and google MAPS API, to create a program with a modern, slick interace where the user signs in to their google account and can search for business using 3 search parameters, which provide a autocomplete function to show the results matching the criteria. The details page returns the selected business details, the maps location and an option to store it to the google users favorites.',
+        title:'YELP APP',
         gif: YELP,
         gitlink:'link'
         
     },
     {
         id:3,
-        description: 'MACHINE LEARNING PROJECT',
+        description: 'Project conducting research to propose and develop different solutions for classification problems using Convolutional Neural Networks, by experimenting with different training approaches including batch training, gradient and stochastic gradient descent methods and different activation and loss functions, augmentation, pooling and dropout. Experiments done within this paper use the Flowers data set from Kaggle, which are plotted and analyzed in order to see evaluate the performance of the different applications and architectures using validation procedures. ',
+        title:'MACHINE LEARNING  PROJECT',
         gif: 'location',
         gitlink:'link'
     },
     {
         id:4,
         description: 'EMAIL TRANSLATE APP',
+        title:'EMAIL TRANSLATE APP',
         gif: 'location',
         gitlink:'link'
     },
     {
       id:5,
-      description: 'EMAIL TRANSLATE APP',
+      description: 'Project utilizing the OMDBApi ( a RESTful web service to obtain movie information), to create a small interface where the user can either view details about the pre-selected movie options, or do a search for details about the movie of their choice.',
+      title:'MOVIE APP',
       gif: MOVIES,
       gitlink:'link'
   }
@@ -59,7 +65,7 @@ class Projects extends Component {
     });
         
     //set state  
-    this.setState({ description: project.description, gif:project.gif });
+    this.setState({ description: project.description, gif:project.gif, title:project.title });
     
   }
 
@@ -75,15 +81,16 @@ class Projects extends Component {
             </div>  
             <div>
               <div className="project-details">
-                <h1 className="text-center mb-5" >{this.state.description}</h1>
-                <img className="float-right" alt="demo gif" src={this.state.gif}></img>
+                <h1 className="text-center mb-5">{this.state.title}</h1>
+                <p className="text-center mb-5 project-description"  >{this.state.description}</p>
+                <img style={{display: 'inline-block'}} className="float-right" alt="demo gif" src={this.state.gif}></img>
               </div>
               <div class="btn-group" role="group" aria-label="Basic example">
                 <button  type="button" className="projectbtn" onClick={() => this.handleClick(1)}> SPORTSHUB</button>
                 <button  type="button" className="projectbtn" onClick={() => this.handleClick(2)}> YELP</button>
+                <button  type="button" className="projectbtn" onClick={() => this.handleClick(5)}> Movie APP</button>
                 <button  type="button" className="projectbtn" onClick={() => this.handleClick(3)}> Machine Learning</button>
                 <button  type="button" className="projectbtn" onClick={() => this.handleClick(4)}> Email Translate</button>
-                <button  type="button" className="projectbtn" onClick={() => this.handleClick(5)}> Movie APP</button>
               </div>
             </div>
         </div>
