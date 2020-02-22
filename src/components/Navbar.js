2534f3f129a3a140508f4950847c6667547d4b5c
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-22 13:03:01 -0500

    just about done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,28 @@ import '../index.css'
 export default function NavBar() {
     return (
     <React.Fragment>
-    <Navbar expand="lg" className="fixed-top my-navbar">
+
+    <Navbar className="fixed-top my-navbar">
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mx-auto">
-            <Nav.Link className="mynavlinks mr-4 ml-4" href="#intro">Intro</Nav.Link>
-            <Nav.Link className="mynavlinks mr-5 ml-5" href="#skills">Skills</Nav.Link>
+        <Nav className="navmargin large-nav">
+            <Nav.Link className="mynavlinks" href="#intro">Intro</Nav.Link>
+            <Nav.Link className="mynavlinks" href="#skills">Skills</Nav.Link>
             <div className="initials mx-auto">DK</div> 
-            <Nav.Link className="mynavlinks ml-5 mr-5" href="#projects">Projects</Nav.Link>
-            <Nav.Link className="mynavlinks ml-5" href="#intro">Contact</Nav.Link>
-
+            <Nav.Link className="mynavlinks ml-2" href="#projects">Projects</Nav.Link>
+            <Nav.Link className="mynavlinks" href="#intro">Contact</Nav.Link>
         </Nav>
+
+        {/* made mobile view a different nav item */}
+        <div>
+            <NavDropdown className="smallnav" title="Nav" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#intro">Intro</NavDropdown.Item>
+                <NavDropdown.Item href="#skills">Skills</NavDropdown.Item>
+                <NavDropdown.Item href="#projects" >Projects</NavDropdown.Item>
+                <NavDropdown.Item  href="#intro">Contact</NavDropdown.Item>
+            </NavDropdown>
+            <div className="initials mobile-initials">DK</div> 
+        </div>
         </Navbar.Collapse>
 
         
